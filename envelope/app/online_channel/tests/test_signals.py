4487f796c5177b498a9a0cd690b71648b9fef8a6
Robin Harms Oredsson <robin@betahaus.net>
2023-10-04 10:29:40 +0200

    online channel tests

--- envelope/app/online_channel/tests/test_signals.py
+++ envelope/app/online_channel/tests/test_signals.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.test import override_settings
 from django.test import TransactionTestCase
 
-from envelope.app.user_channel.channel import UserChannel
+from envelope.app.online_channel.channel import OnlineChannel
 from envelope.envelopes import outgoing
 from envelope.messages.common import Status
 from envelope.tests.helpers import get_consumer_name
@@ -23,25 +23,11 @@ class SubscribeSignalIntegrationTests(TransactionTestCase):
         self.client.force_login(self.jane)
 
     async def test_connection_subscribes(self):
-        communicator = await mk_communicator(self.client, drain=False)
-        response = await communicator.receive_from()
-        payload = outgoing.parse(response)
-        self.assertEqual(
-            {
-                "t": "channel.subscribed",
-                "p": {
-                    "pk": self.jane.pk,
-                    "channel_type": "user",
-                    "channel_name": f"user_{self.jane.pk}",
-                    "app_state": None,
-                },
-            },
-            payload.dict(exclude_none=True),
-        )
+        communicator = await mk_communicator(self.client)
         layer = get_channel_layer()
-        self.assertIn(f"user_{self.jane.pk}", layer.groups)
+        self.assertIn(OnlineChannel.channel_name, layer.groups)
         consumer_name = await get_consumer_name(communicator)
-        ch = UserChannel(self.jane.pk, consumer_channel=consumer_name)
+        ch = OnlineChannel(consumer_channel=consumer_name)
         msg = Status(state="s")
         await ch.publish(msg)
         response = await communicator.receive_from()
@@ -53,8 +39,8 @@ class SubscribeSignalIntegrationTests(TransactionTestCase):
         await communicator.disconnect()
 
     async def test_disconnect_leaves(self):
-        communicator = await mk_communicator(self.client, drain=False)
+        communicator = await mk_communicator(self.client)
         layer = get_channel_layer()
-        self.assertIn(f"user_{self.jane.pk}", layer.groups)
+        self.assertIn(OnlineChannel.channel_name, layer.groups)
         await communicator.disconnect()
-        self.assertNotIn(f"user_{self.jane.pk}", layer.groups)
+        self.assertNotIn(OnlineChannel.channel_name, layer.groups)
